cd1ca4d94a2c8106b292a58d6e7b2a2d0d666c0b
barryyim <bbarryyim@gmail.com>
2019-12-13 19:19:21 -0800

    Adding the post method for person model

--- api/items.py
+++ api/items.py
@@ -1,12 +1,41 @@
-items = {
-    0: {"name": "First item"},
-    1: {"name": "Second item"},
-    2: {"name": "third item"},
-    3: {"name": "forth item"},
-    4: {"name": "fifth item"},
-    5: {"name": "six item"}
+from datetime import datetime
+
+
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+PERSON = {
+    "Farrell": {
+        "fname": "Doug",
+        "lname": "Farrell",
+        "timestamp": get_timestamp(),
+    },
+    "Brockman": {
+        "fname": "Kent",
+        "lname": "Brockman",
+        "timestamp": get_timestamp(),
+    },
+    "Easter": {
+        "fname": "Bunny",
+        "lname": "Easter",
+        "timestamp": get_timestamp(),
+    },
 }
 
 
 def search() -> list:
-    return items
+    return PERSON
+
+
+def create(person):
+    lname = person.get('lname', None)
+    fname = person.get('fname', None)
+    timestamp = get_timestamp()
+
+    if lname not in PERSON and lname is not None:
+        PERSON[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp()
+        }
