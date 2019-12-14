bec2cc9819e1503e196f4cdbed978030e4c4d480
barryyim <bbarryyim@gmail.com>
2019-12-13 17:33:47 -0800

    Initial commit

---
+++ api/items.py
@@ -0,0 +1,12 @@
+items = {
+    0: {"name": "First item"},
+    1: {"name": "Second item"},
+    2: {"name": "third item"},
+    3: {"name": "forth item"},
+    4: {"name": "fifth item"},
+    5: {"name": "six item"}
+}
+
+
+def search() -> list:
+    return items
---
+++ app.py
@@ -0,0 +1,7 @@
+from connexion.resolver import RestyResolver
+import connexion
+
+if __name__ == '__main__':
+    app = connexion.App(__name__, host='localhost', port=8080, specification_dir='swagger/')
+    app.add_api('my_app.yaml', resolver=RestyResolver('api'))
+    app.run()
